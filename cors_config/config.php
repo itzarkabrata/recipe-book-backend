902310db3b3b57efd382160190ae5b357d75f48f
itzarkabrata <arkabrata1292@gmail.com>
2024-09-29 21:22:00 +0530

    allow origin multiple

--- cors_config/config.php
+++ cors_config/config.php
@@ -1,5 +1,20 @@
 <?php
-$react_app_host_url = "https://recipe-book-nine-swart.vercel.app";
 
-header("Access-Control-Allow-Origin: ".$react_app_host_url);
+$allowed_origins = [
+    "http://localhost:5173",
+    "https://recipe-book-nine-swart.vercel.app"
+];
+
+// Get the origin of the incoming request
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+
+if (in_array($origin, $allowed_origins)){
+    header("Access-Control-Allow-Origin: $origin");
+}
+else{
+    header('HTTP/1.1 403 Forbidden');
+    echo "Access denied!";
+    exit;
+}
+
 ?>
