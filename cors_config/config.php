44e8887cab3f31a7acb311d1b5e016257ca61039
itzarkabrata <arkabrata1292@gmail.com>
2024-09-29 21:13:20 +0530

    allow origin again

--- cors_config/config.php
+++ cors_config/config.php
@@ -1,5 +1,5 @@
 <?php
-$react_app_host_url = "https://recipe-book-nine-swart.vercel.app/";
+$react_app_host_url = "https://recipe-book-nine-swart.vercel.app";
 
 header("Access-Control-Allow-Origin: ".$react_app_host_url);
 ?>
